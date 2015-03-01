550baf294848a36377f5bd1ca879f26ed0782a19
Tommy Kananen <tkananen@udel.edu>
2015-02-28 19:49:35 -0500

    testing.

---
+++ src/MainPackage/tuitionRate.java
@@ -0,0 +1,27 @@
+package MainPackage;
+
+import java.util.Scanner;
+
+public class tuitionRate {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Please enter the tuition amount: ");
+		double initTuition = input.nextDouble();
+
+		System.out
+				.print("Please enter the rate your tution will increase as a whole number: ");
+		double percentageInc = input.nextDouble();
+		double tuitRate = percentageInc / 100;
+		double tuitionSum = 0;
+
+		for (int j = 1; j <= 4; j++) {
+			tuitionSum = tuitionSum + (initTuition * Math.pow(10, tuitRate));
+
+			
+
+		}
+		System.out.printf("After four years, your  total tuition costs comes to: $%.2f%n ", tuitionSum);
+
+	}
+}
